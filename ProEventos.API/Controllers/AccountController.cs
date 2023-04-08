33e06cbc670405b96b316c31d44e4241a9929ea8
Cristiano Andrade <candrades@indracompany.com>
2023-04-08 15:57:28 -0300

    217

--- ProEventos.API/Controllers/AccountController.cs
+++ ProEventos.API/Controllers/AccountController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using ProEventos.Application.Dtos;
 using ProEventos.Application.Services.Interfaces;
 using System;
 using System.Threading.Tasks;
@@ -25,12 +26,34 @@ namespace ProEventos.API.Controllers
         }
 
         [HttpGet("GetUser/{userName}")]
+        [AllowAnonymous]
         public async Task<IActionResult> GetUser(string userName)
         {
             try
             {
                 var user = await _accountService.GetUserByUserNameAsync(userName);
                 return Ok(user);
+            }
+            catch (Exception ex)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, $"Erro ao tentar recuperar usuário. Erro: {ex.Message}");
+            }
+        }
+
+        [HttpPost("Register")]
+        [AllowAnonymous]
+        public async Task<IActionResult> Register(UserDto userDto)
+        {
+            try
+            {
+                if (await _accountService.UserExists(userDto.Username))
+                     return BadRequest("Usuário já existe");
+
+                var uuser = await _accountService.CreateAccountAsync(userDto);
+                if (uuser != null)
+                    return Ok(uuser);
+
+                return BadRequest("Usuário não criado, tente novamente mais tarde!");
             }
             catch (Exception ex)
             {
--- ProEventos.Application/Helpers/ProEventoProfile.cs
+++ ProEventos.Application/Helpers/ProEventoProfile.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using ProEventos.Application.Dtos;
+using ProEventos.Domain.Identity;
 using ProEventos.Domain.Models;
 
 namespace ProEventos.Application.Helpers
@@ -12,6 +13,7 @@ namespace ProEventos.Application.Helpers
             CreateMap<Lote, LoteDto>().ReverseMap();
             CreateMap<Palestrante, PalestranteDto>().ReverseMap();
             CreateMap<RedeSocial, RedeSocialDto>().ReverseMap();
+            CreateMap<User, UserDto>().ReverseMap();
         }
     }
 }
